dbb8d572503a95674c79613bdb55722b976f64aa
katagaki <68231035+katagaki@users.noreply.github.com>
2026-03-02 00:53:41 +0900

    Set up project

---
+++ Widgets/WidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by シン・ジャスティン on 2026/03/02.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+    }
+}
