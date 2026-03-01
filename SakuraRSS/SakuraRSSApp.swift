8f1f98884cf42b63214f87a74b0f25248828e6d2
katagaki <68231035+katagaki@users.noreply.github.com>
2026-03-02 00:43:04 +0900

    Initial Commit

---
+++ SakuraRSS/SakuraRSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SakuraRSSApp.swift
+//  SakuraRSS
+//
+//  Created by シン・ジャスティン on 2026/03/02.
+//
+
+import SwiftUI
+
+@main
+struct SakuraRSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
