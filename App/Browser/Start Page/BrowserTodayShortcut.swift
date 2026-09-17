df5cd70a48788bee043176c26b199b293907c1ef
katagaki <68231035+katagaki@users.noreply.github.com>
2026-09-17 09:14:03 +0900

    Tint the Today shortcut glyphs with the app tint

--- App/Browser/Start Page/BrowserTodayShortcut.swift
+++ App/Browser/Start Page/BrowserTodayShortcut.swift
@@ -29,13 +29,4 @@ enum BrowserTodayShortcut: String, CaseIterable, Identifiable {
         }
     }
 
-    /// Tints the icon tile the way a list's icon tints its own in Following.
-    var tint: Color {
-        switch self {
-        case .following: .blue
-        case .allContent: .orange
-        case .bookmarks: .pink
-        case .topics: .purple
-        }
-    }
 }
