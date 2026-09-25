219ed559bdd132c6e7995e0d428ca92cce04d3d1
katagaki <68231035+katagaki@users.noreply.github.com>
2026-09-26 08:04:30 +0900

    Refactor EnhancedNavigation package

--- Enhanced Navigation/Sources/EnhancedNavigation/Model/BackHistoryEntry.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-/// A page behind the current one, with the depth to pop back to.
-public struct BackHistoryEntry<Identity: TabPageIdentity> {
-    public let depth: Int
-    public let identity: Identity
-}
